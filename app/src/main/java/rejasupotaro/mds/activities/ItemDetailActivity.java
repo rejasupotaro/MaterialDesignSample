fddbc805b856fc55ea7d2667b135ac041b08ed17
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-05-06 14:17:40 +0900

    Use Palette

--- app/src/main/java/rejasupotaro/mds/activities/ItemDetailActivity.java
+++ app/src/main/java/rejasupotaro/mds/activities/ItemDetailActivity.java
@@ -2,10 +2,14 @@ package rejasupotaro.mds.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,8 +24,12 @@ public class ItemDetailActivity extends BaseActivity {
 
     @InjectView(R.id.item_image)
     ImageView itemImageView;
+    @InjectView(R.id.title_wrapper)
+    View titleWrapperView;
     @InjectView(R.id.title_text)
     TextView titleTextView;
+    @InjectView(R.id.updated_at_text)
+    TextView updatedAtTextView;
     @InjectView(R.id.user_image)
     ImageView userImageView;
 
@@ -47,6 +55,18 @@ public class ItemDetailActivity extends BaseActivity {
 
     private void setupViews() {
         titleTextView.setText("Title");
-        userImageView.setImageResource(R.drawable.user);
+
+        Drawable userDrawable = getResources().getDrawable(R.drawable.user);
+        userImageView.setImageDrawable(userDrawable);
+        Bitmap userBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.user);
+        new Palette.Builder(userBitmap)
+                .generate(palette -> {
+                    int darkMutedColor = palette.getDarkMutedColor(R.color.text_color_primary);
+                    titleWrapperView.setBackgroundColor(darkMutedColor);
+                    int vibrantColor = palette.getVibrantColor(android.R.color.white);
+                    titleTextView.setTextColor(vibrantColor);
+                    int lightVibrantColor = palette.getLightVibrantColor(R.color.text_color_tertiary);
+                    updatedAtTextView.setTextColor(lightVibrantColor);
+                });
     }
 }
